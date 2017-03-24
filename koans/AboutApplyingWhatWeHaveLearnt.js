ee970d3ce5bedf27391f9b0e97be7aba75b750dd
doug <dougecox@gmail.com>
2017-03-24 13:14:14 -0700

    Add files via upload

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -30,15 +30,31 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
+      productsICanEat = _(products).filter(function () {
+            var i,j,hasMushrooms, productsICanEat = [];
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+            for (i = 0; i < products.length; i+=1) {
+                if (products[i].containsNuts === false) {
+                    hasMushrooms = false;
+                    for (j = 0; j < products[i].ingredients.length; j+=1) {
+                       if (products[i].ingredients[j] === "mushrooms") {
+                          hasMushrooms = true;
+                       }
+                    }
+                    if (!hasMushrooms) productsICanEat.push(products[i]);
+                }
+            }
+            return productsICanEat;
+          } );
+
+      expect(productsICanEat.length).toBe(5);
   });
 
   /*********************************************************************************/
@@ -52,13 +68,18 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = _.range(1, 1000).reduce(function(sum, num) {
+      if (num % 3 === 0 || num % 5 === 0 ) {
+        sum += num;
+      } 
+      return sum;
+    }, 0);    /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -71,26 +92,65 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
-    var ingredientCount = { "{ingredient name}": 0 };
+    var ingredientCount = 
 
-    /* chain() together map(), flatten() and reduce() */
+    /* chain() together map(), flatten() and reduce() 
+     */
+      _(products).chain()
+                 .map(function (name) {
+                  return name.ingredients; })
+                 .flatten()
+                 .reduce(function(ingredientCount, ingredient) {
+                  ingredientCount[ingredient] = (ingredientCount[ingredient] || 0) + 1;
+                  return ingredientCount }, {} )
+                 .value();
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+                
+
+
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
   /* UNCOMMENT FOR EXTRA CREDIT */
-  /*
-  it("should find the largest prime factor of a composite number", function () {
+var isPrime = function(value) {
+    if (value < 2 || value !==  Math.round(value) ) {
+      return false;
+    }
+    for (var i = 2; i < value; i ++) {
+      if (n % i === 0) { return false; }
+    }
+    return true;
+  };
   
-  });
+  it("should find the largest prime factor of a composite number", function () {
+    var composite = function(value) {
+      for (var value; value > 0; value--) {
+        if (isPrime(value) ) { return value; }
+      }
+    };
+
+  
+
+
+  expect(9973).toBe(composite(10000));
+
 
   it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
-    
+    var isPalindrome = function(value) {
+      var num = value;
+      return ( value === parseInt(num.toString().split("").reverse().join("")); )
+    }
+
+    var largestPalindromeProdofTwoThreeDigitNum = function( number1, number2 ) {
+      var largestNum;
+      var num1 
+    }
+
   });
 
   it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
@@ -105,5 +165,5 @@ describe("About Applying What We Have Learnt", function() {
   it("should find the 10001st prime", function () {
 
   });
-  */
+  
 });
